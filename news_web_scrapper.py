7131ff5c8bddb97a5997ee991ecb61acb8e8dc90
unknown <brannongarrett77@gmail.com>
2023-09-25 15:09:51 +0200

    Added imports and code

--- news_web_scrapper.py
+++ news_web_scrapper.py
@@ -1,6 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
 # News webscrapper
 def main():
-
+url = "https://www.example.com/news"
+response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, "html.parser")
 
 # main function
 if __name__ == "__main__":
